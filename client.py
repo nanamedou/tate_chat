9ba5fce5fe1dd08a467cbc709e140333934c587a
nanamedou <45792258+nanamedou@users.noreply.github.com>
2021-10-14 00:54:20 +0900

    通信部分をasyncioに変更

--- client.py
+++ client.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import socket
+import asyncio
 
-HOST = 'localhost'
-PORT = 51234
+HOST = 'localhost'  # ホストのアドレス
+PORT = 51234        # ホストのポート
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.send(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+# メッセージを特定のアドレスポートに送信する
+async def post_message(message):
+    # 接続を確立する
+    reader, writer = await asyncio.open_connection(HOST, PORT)
+
+    # メッセージを送信し、その後送信終了
+    writer.write(message)
+    await writer.drain()
+    writer.close()
+
+    # 相手が全てのデータを送信完了するまで
+    # 受信を続ける
+    data = bytes()
+    while(not reader.at_eof()):
+        data += await reader.read(1024)
+
+    # 受信したデータを表示
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f'Received {message} from {addr}')
+
+
+async def main():
+    # 入力されたメッセージを送信する
+    message = input('メッセージを入力:').encode()
+    await post_message(message)
+
+
+while True:
+    asyncio.run(main())
--- host.py
+++ host.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import socket
+import asyncio
 
-HOST = ''
-PORT = 51234
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print('Received', repr(data))
-            conn.sendall(data)
+HOST = ''       # 作成するtサーバのアドレス
+PORT = 51234    # 作成するtサーバのポート
+
+
+# サーバに送られたメッセージをそのまま返送するコールバック関数
+async def echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+
+    # 相手が全てのデータを送信完了するまで
+    # 読み取りを続ける
+    data = bytes()
+    while(not reader.at_eof()):
+        data += await reader.read(1024)
+
+    # 受信したデータを表示
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f'Received {message} from {addr}')
+
+    # 返送を送信する
+    writer.write(data)
+    await writer.drain()
+
+    # 送信終了
+    writer.close()
+
+
+async def main():
+
+    # サーバーを作成
+    server = await asyncio.start_server(echo, HOST, PORT)
+    addr = server.sockets[0].getsockname()
+    print(f'Ruinning Server {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
